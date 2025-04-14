809c49cef1ffa05b2ccf9548afe9dcf3451bb256
caarloosdab <caarloosdab@gmail.com>
2025-04-14 12:04:23 -0600

    delete

--- routes/goals.js
+++ routes/goals.js
@@ -6,8 +6,8 @@ const { isAuthenticated } = require('../middleware/authenticate');
 
 router.get('/', goalsController.getAll);
 router.get('/:id', goalsController.getSingle);
-router.post('/', isAuthenticated, goalsController.creategoal);
-router.put('/:id', isAuthenticated, goalsController.updategoal);
+router.post('/', isAuthenticated, goalsController.validateGoal ,goalsController.creategoal);
+router.put('/:id', isAuthenticated, goalsController.validateGoal ,goalsController.updategoal);
 router.delete('/:id', isAuthenticated, goalsController.deletegoal);
 
 module.exports = router;
